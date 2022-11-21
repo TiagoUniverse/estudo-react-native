734139031b4a7232f04bf911a356ff90c0cd2624
Tiago Cesar da Silva Lopes <tiagolopes@pe.senac.br>
2022-11-21 16:40:25 -0300

    Retornando o projeto

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require ('./assets/image_maker_te6J43SJkSC9.png')} />
+      <Text>Tiago César está na área! Bem vindo ao meu app</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
