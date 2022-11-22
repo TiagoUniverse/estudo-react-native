a7dc2bcb92972e903e5676c6ee773bff930b9122
Tiago Cesar da Silva Lopes <tiagolopes@pe.senac.br>
2022-11-22 13:45:33 -0300

    Adicionei imagem

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import logo from "./assets/image_maker_te6J43SJkSC9.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={require ('./assets/image_maker_te6J43SJkSC9.png')} />
+
+      <Image source={logo} style={{ width: 305, height: 159}} />
+
       <Text>Tiago César está na área! Bem vindo ao meu app</Text>
       <StatusBar style="auto" />
     </View>
